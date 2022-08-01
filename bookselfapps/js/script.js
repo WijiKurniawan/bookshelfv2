d98ca4fdb5d0e4a5a41954d0088dbb9531c4e9fc
WijiKurniawan <wijikurniawan121@gmail.com>
2022-08-02 00:19:49 +0700

    menampilkan list buku

--- bookselfapps/js/script.js
+++ bookselfapps/js/script.js
@@ -1,185 +1,155 @@
-//menambah aksi untuk todo start
-document.addEventListener('DOMContentLoaded',function () {// aksi
-    const submitFrom = document.getElementById('form');
-    submitFrom.addEventListener('submit', function (event) {
-        event.preventDefault();
-        addTodo();// dibbungkus disini
-    }); 
-});
-//end
-
-//buat fungsi todo start 
-function addTodo() {
-    const textTodo = document.getElementById('title').value;
-    const timestamp = document.getElementById('date').value;
-
-    const generatedID = generateId();
-    const todoObject = generateTodoObject(generatedID,textTodo,timestamp,false);
-    todos.push(todoObject);
-
-    document.dispatchEvent(new Event(RENDER_EVENT));
-}
-//end
-
-//menuliskan fungsi start
-function generateId() {// untuk menghasilkan id
-    return +new Date();//fungsi new Date untuk mengambil timestamp pada js
-}
-function generateTodoObject(id,task,timestamp,isCompleted) {//fubgsi untuk meng generate Todo yang baru
-    return{
-        id,
-        task,
-        timestamp,
-        isCompleted
-    }
-}
-//end
-
-//implementasi start
-const todos = [];//array yang berfungsi menampung data Todo
-const RENDER_EVENT = 'render-todo';// custom event yang berfungsi merubah data pada todo(patokan/default)
-//end
-
-//buat listener RENDER_EVENT
-document.addEventListener(RENDER_EVENT,function () {
-    console.log(todos)//mencoba listener dengan mencetak todo
+document.addEventListener('DOMContentLoaded', function () {
+  const submitForm = document.getElementById('form');
+  submitForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+    addbook();
+  });
 });
 
-//pembuatan elemen yang akan di tampilakan di halaman web
-function makeTodo(todoObject) {
-    const textTitle = document.createElement('h2');
-    textTitle.innerText = todoObject.task;
-   
-    const textTimestamp = document.createElement('p');
-    textTimestamp.innerText = todoObject.timestamp;
-   
-    const textContainer = document.createElement('div');
-    textContainer.classList.add('inner');
-    textContainer.append(textTitle, textTimestamp);
-   
-    const container = document.createElement('div');
-    container.classList.add('item', 'shadow');
-    container.append(textContainer);
-    container.setAttribute('id', `todo-${todoObject.id}`);
+function addbook() {
+  const inputBooktitle = document.getElementById('inputBooktitle').value;
+  const inputBookAuthor = document.getElementById('inputBookAuthor').value;
+  const inputyear = document.getElementById('inputyear').value;
 
-    if (todoObject.isCompleted) {
-        const undoButton = document.createElement('button');
-        undoButton.classList.add('undo-button');
-     
-        undoButton.addEventListener('click', function () {
-          undoTaskFromCompleted(todoObject.id);
-        });
-     
-        const trashButton = document.createElement('button');
-        trashButton.classList.add('trash-button');
-     
-        trashButton.addEventListener('click', function () {
-          removeTaskFromCompleted(todoObject.id);
-        });
-     
-        container.append(undoButton, trashButton);
-      } else {
-        const checkButton = document.createElement('button');
-        checkButton.classList.add('check-button');
-        
-        checkButton.addEventListener('click', function () {
-          addTaskToCompleted(todoObject.id);
-        });
-        
-        container.append(checkButton);
-      }
-   
-    return container;
+  const generatedID = generateId();
+  const bookObject = generateBookObject(generatedID, inputBooktitle, inputBookAuthor,inputyear, false);
+  books.push(bookObject);
+
+  document.dispatchEvent(new Event(RENDER_EVENT));
+  
+}
+
+function generateId() {
+  return +new Date();
+}
+
+function generateBookObject(id,title,author,year,isCompleted) {
+  return{
+    id,
+    title,
+    author,
+    year,
+    isCompleted,
   }
 
-//render Event
-  document.addEventListener(RENDER_EVENT, function () {
-    const uncompletedTODOList = document.getElementById('todos');
-    uncompletedTODOList.innerHTML = '';
-   
-    const completedTODOList = document.getElementById('completed-todos');
-    completedTODOList.innerHTML = '';
+}
+const books = [];
+const RENDER_EVENT = 'render-book';
 
-    for (const todoItem of todos) {
-        const todoElement = makeTodo(todoItem);
-        if (!todoItem.isCompleted)
-          uncompletedTODOList.append(todoElement);
-        else
-          completedTODOList.append(todoElement);
-      }
+document.addEventListener(RENDER_EVENT, function () {
+  console.log(books);
+});
+
+function makeBook(bookObject) {
+  const inputBooktitle = document.createElement('h2');
+  inputBooktitle.innerText = bookObject.title;
+
+  const inputBookAuthor = document.createElement('em');
+  inputBookAuthor.innerText = bookObject.author;
+
+  const inputyear = document.createElement('p');
+  inputyear.innerText = bookObject.year;
+
+  const textContainer = document.createElement('div');
+  textContainer.classList.add('inner');
+  textContainer.append(inputBooktitle, inputBookAuthor,inputyear);
+
+  const container = document.createElement('div');
+  container.classList.add('item', 'shadow');
+  container.append(textContainer);
+  container.setAttribute('id', `book-${bookObject.id}`);
+
+  if (bookObject.isCompleted) {
+    const undoButton = document.createElement('button');
+    undoButton.classList.add('undo-button');
+
+    undoButton.addEventListener('click', function () {
+      undoBookFromCompleted(bookObject.id);
     });
-// render event
 
-// function ceklis Todo ke yang sudah dilakukan start 
-  function addTaskToCompleted (todoId) {
-    const todoTarget = findTodo(todoId);
-   
-    if (todoTarget == null) return;
-   
-    todoTarget.isCompleted = true;
-    document.dispatchEvent(new Event(RENDER_EVENT));
+    const trashButton = document.createElement('button');
+    trashButton.classList.add('trash-button');
+
+    trashButton.addEventListener('click', function () {
+      removeBookFromCompleted(bookObject.id);
+    });
+
+    container.append(undoButton, trashButton);
+  } else {
+    const checkButton = document.createElement('button');
+    checkButton.classList.add('check-button');
+
+    checkButton.addEventListener('click', function () {
+      addBookToCompleted(bookObject.id);
+    });
+    
+    container.append(checkButton);
   }
 
-  function findTodo(todoId) {
-    for (const todoItem of todos) {
-      if (todoItem.id === todoId) {
-        return todoItem;
-      }
+  return container;
+}
+
+document.addEventListener(RENDER_EVENT, function () {
+  const uncompletedBOOKList = document.getElementById('books');
+  uncompletedBOOKList.innerHTML = '';
+
+  const completedBOOKList = document.getElementById('completed-books');
+  completedBOOKList.innerHTML = '';
+
+  for (const bookItem of books) {
+    const bookElement = makeBook(bookItem);
+    if (!bookItem.isCompleted) 
+      uncompletedBOOKList.append(bookElement);
+    else
+      completedBOOKList.append(bookElement); 
+  }
+});
+
+function addBookToCompleted (bookId) {
+  const bookTarget = findBook(bookId);
+ 
+  if (bookTarget == null) return;
+ 
+  bookTarget.isCompleted = true;
+  document.dispatchEvent(new Event(RENDER_EVENT));
+}
+
+function findBook(bookId) {
+  for (const bookItem of books) {
+    if (bookItem.id === bookId) {
+      return bookItem;
     }
-    return null;
   }
-// end
+  return null;
+}
 
-//function untuk hapus dan undo Todo start
-function removeTaskFromCompleted(todoId) {
-    const todoTarget = findTodoIndex(todoId);
-   
-    if (todoTarget === -1) return;
-   
-    todos.splice(todoTarget, 1);
-    document.dispatchEvent(new Event(RENDER_EVENT));
-  }
-   
-   
-  function undoTaskFromCompleted(todoId) {
-    const todoTarget = findTodo(todoId);
-   
-    if (todoTarget == null) return;
-   
-    todoTarget.isCompleted = false;
-    document.dispatchEvent(new Event(RENDER_EVENT));
-  }
-// end
+//function utk remove
+function removeBookFromCompleted(bookId) {
+  const bookTarget = findBookIndex(bookId);
+ 
+  if (bookTarget === -1) return;
 
-// function untuk Todo hapus dan undo
-function removeTaskFromCompleted(todoId) {
-    const todoTarget = findTodoIndex(todoId);
-   
-    if (todoTarget === -1) return;
-   
-    todos.splice(todoTarget, 1);
-    document.dispatchEvent(new Event(RENDER_EVENT));
-  }
-   
-   
-  function undoTaskFromCompleted(todoId) {
-    const todoTarget = findTodo(todoId);
-   
-    if (todoTarget == null) return;
-   
-    todoTarget.isCompleted = false;
-    document.dispatchEvent(new Event(RENDER_EVENT));
-  }
-// end
+  books.splice(bookTarget, 1);
+  document.dispatchEvent(new Event(RENDER_EVENT));
+}
 
-// fix error fungsi find todoIndex tidak ditemukan
-function findTodoIndex(todoId) {
-    for (const index in todos) {
-      if (todos[index].id === todoId) {
-        return index;
-      }
+function undoBookFromCompleted(bookId) {
+  const bookTarget = findBook(bookId);
+
+  if (bookTarget == null) return;
+
+  bookTarget.isCompleted = false;
+  document.dispatchEvent(new Event(RENDER_EVENT));
+}
+
+//buat fungsi findBookIndex
+function findBookIndex(bookId) {
+  for (const index in books) {
+    if (books[index].id === bookId) {
+      return index;
     }
-   
-    return -1;
   }
-  // selesai buat function findTodoIndex
+
+  return -1;
+}
+
